8441fb35de80353c3621cdb7dfb6579fc98dd33f
David Amedi <amukoahdavid@gmail.com>
2025-05-25 22:32:27 +0300

    Implemented employee-related CLI functions (options 7-13)

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,4 +1,7 @@
 import sqlite3
+import os
 
-CONN = sqlite3.connect('company.db')
+# Use absolute path to lib/company.db
+db_path = os.path.join(os.path.dirname(__file__), '..', 'company.db')
+CONN = sqlite3.connect(db_path)
 CURSOR = CONN.cursor()
